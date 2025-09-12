3cd6dabb766fea66069e8d0f07141d9363b5eb44
nahuel61920 <estebancarrizo619@gmail.com>
2025-09-12 02:54:41 -0300

    feat: :sparkles: Peticiones para ventas local

--- routes/api.php
+++ routes/api.php
@@ -203,6 +203,8 @@ Route::middleware('jwt.auth')->prefix('sales')->group(function () {
     Route::put('/internal_comments/{id}', [SaleController::class, 'updateInternalComment']);
     Route::put('/client-data/{id}', [SaleController::class, 'updateClientData']);
     Route::put('/assign-client/{id}', [SaleController::class, 'assignUser']);
+    Route::post('/local', [SaleController::class, 'storeLocalSale']);
+    Route::put('/local/{id}', [SaleController::class, 'updateLocalSale']);
 });
 
 // Shipping options
