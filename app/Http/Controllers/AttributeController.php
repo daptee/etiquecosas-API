bf3f83e0134176be5a0a5a8a342013013738fe10
nahuel61920 <estebancarrizo619@gmail.com>
2025-10-21 13:19:12 -0300

    refactor: :recycle: editar attribute value

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Traits\FindObject;
 use App\Traits\ApiResponse;
-use App\Traits\Auditable; 
+use App\Traits\Auditable;
 
 class AttributeController extends Controller
 {
@@ -83,58 +83,65 @@ class AttributeController extends Controller
     public function update(Request $request, $id)
     {
         $attribute = $this->findObject(Attribute::class, $id);
+
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255|unique:attributes,name,' . $attribute->id,
             'statusId' => 'nullable|in:1,2',
-            'values' => 'nullable|array|',
+            'values' => 'nullable|array',
             'values.*.id' => 'nullable|integer|exists:attribute_values,id',
             'values.*.value' => 'required|string|max:255',
             'values.*.statusId' => 'nullable|in:1,2',
         ]);
+
         if ($validator->fails()) {
             $this->logAudit(Auth::user(), 'Update Attribute', $request->all(), $validator->errors());
             return $this->validationError($validator->errors());
         }
 
-        $attribute->name = $request->input('name', $attribute->name);
-        $attribute->status_id = $request->input('statusId', $attribute->status_id);
+        // Actualizamos el atributo principal
+        $attribute->update([
+            'name' => $request->input('name', $attribute->name),
+            'status_id' => $request->input('statusId', $attribute->status_id),
+        ]);
+
+        // Procesamos los valores
+        $submittedValues = collect($request->input('values', []));
         $existingIds = $attribute->values()->pluck('id')->toArray();
-        $submittedValues = collect($request->input('values'));
         $submittedIds = $submittedValues->pluck('id')->filter()->toArray();
+
+        // Detectar eliminados
         $valuesToDelete = array_diff($existingIds, $submittedIds);
-        $valuesToUpdate = [];
-        $valuesToCreate = [];
+
+        // Actualizar y crear
         foreach ($submittedValues as $data) {
             if (!empty($data['id']) && in_array($data['id'], $existingIds)) {
-                $valuesToUpdate[$data['id']] = [
+                // Actualizar value existente
+                $attribute->values()->where('id', $data['id'])->update([
                     'value' => $data['value'],
                     'status_id' => $data['statusId'] ?? 1,
-                ];
+                ]);
             } else {
-                $valuesToCreate[] = [
+                // Crear nuevo value
+                $attribute->values()->create([
                     'value' => $data['value'],
                     'status_id' => $data['statusId'] ?? 1,
-                ];
+                ]);
             }
         }
-        foreach ($valuesToUpdate as $valueId => $updateData) {
-            $attribute->values()->where('id', $valueId)->update($updateData);
-        }
 
-        if (!empty($valuesToCreate)) {
-            $attribute->values()->createMany($valuesToCreate);
-        }
-
+        // Eliminar los que no se enviaron
         if (!empty($valuesToDelete)) {
             $attribute->values()->whereIn('id', $valuesToDelete)->delete();
         }
 
+        // Recargar relaciones
         $attribute->load('values.generalStatus', 'generalStatus');
+
         $this->logAudit(Auth::user(), 'Update Attribute', $request->all(), $attribute);
+
         return $this->success($attribute, 'Atributo actualizado');
     }
-
-
+    
     public function delete($id)
     {
         $attribute = $this->findObject(Attribute::class, $id);
