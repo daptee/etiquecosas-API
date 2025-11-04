66d090a1da97973fa6d3f6709935990e93d53ace
nahuel61920 <estebancarrizo619@gmail.com>
2025-11-04 15:58:09 -0300

    refactor: :recycle: ->first()

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -32,7 +32,7 @@ class AttributeController extends Controller
         $query->orderBy('name', 'asc');
         if (!$perPage) {
             $attributes = $query->get();
-            $this->logAudit(Auth::user(), 'Get Attributes List', $request->all(), $attributes);
+            $this->logAudit(Auth::user(), 'Get Attributes List', $request->all(), $attributes->first());
             return $this->success($attributes, 'Atributos obtenidos');
         }
 
