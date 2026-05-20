cddb420e8c6505ebf53987a62780c6f4fd1bb4ed
nahuel61920 <estebancarrizo619@gmail.com>
2026-05-20 15:27:38 -0300

    refactor: :recycle: optional client

--- app/Http/Middleware/OptionalClientAuth.php
+++ app/Http/Middleware/OptionalClientAuth.php
@@ -4,17 +4,18 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class OptionalClientAuth
 {
     public function handle(Request $request, Closure $next)
     {
         try {
-            $token = JWTAuth::parseToken();
-            $user = $token->authenticate();
-            if ($user) {
-                auth()->guard('client')->setUser($user);
+            $token = $request->bearerToken();
+            if ($token) {
+                $client = auth('client')->setToken($token)->authenticate();
+                if ($client) {
+                    auth('client')->setUser($client);
+                }
             }
         } catch (\Throwable $e) {
             // Sin token o token inválido — continúa sin autenticar
