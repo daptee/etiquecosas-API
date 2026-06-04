63e28ec3d38c5e4b503878f8c2a7080dc8344ff0
nahuel61920 <estebancarrizo619@gmail.com>
2026-06-04 16:01:46 -0300

    refactor: :recycle: fallida stock_movements

--- database/migrations/2026_06_04_203958_add_channel_id_to_stock_movements_table.php
+++ database/migrations/2026_06_04_203958_add_channel_id_to_stock_movements_table.php
@@ -8,8 +8,13 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (!Schema::hasColumn('stock_movements', 'channel_id')) {
+            Schema::table('stock_movements', function (Blueprint $table) {
+                $table->bigInteger('channel_id')->nullable()->after('sale_id');
+            });
+        }
+
         Schema::table('stock_movements', function (Blueprint $table) {
-            $table->bigInteger('channel_id')->nullable()->after('sale_id');
             $table->foreign('channel_id')->references('id')->on('channels')->nullOnDelete();
         });
     }
